686980ae3ef97fb7de496b19455c4248c08975f2
legalizemath <89911237+legalizemath@users.noreply.github.com>
2021-09-03 17:49:55 -0400

    less messy

--- bos.js
+++ bos.js
@@ -1,32 +1,37 @@
 /*
   Wrapper for balanceofsatoshis installed globally
   linked via `npm link balanceofsatoshis`
+  Used with bos v10.10.2
 */
 
-import bos_network from 'balanceofsatoshis/network/index.js'
 import { callRawApi } from 'balanceofsatoshis/commands/index.js'
-import { adjustFees } from 'balanceofsatoshis/routing/index.js'
-import { rebalance } from 'balanceofsatoshis/swaps/index.js'
+import { adjustFees as bosAdjustFees } from 'balanceofsatoshis/routing/index.js'
+import { rebalance as bosRebalance } from 'balanceofsatoshis/swaps/index.js'
 import { readFile } from 'fs'
 import lnd from 'balanceofsatoshis/lnd/index.js'
+import {
+  pushPayment as bosPushPayment,
+  reconnect as bosReconnect,
+  getPeers as bosGetPeers
+} from 'balanceofsatoshis/network/index.js'
 
 // use existing global bos authentication
 const authenticatedLnd = lnd.authenticatedLnd({})
 
-const runReconnect = async (log = false) => {
+const reconnect = async (log = false) => {
   try {
-    log && console.log(`${getDate()} bos.runReconnect()`)
-    const res = await bos_network.reconnect({
+    log && console.log(`${getDate()} bos.reconnect()`)
+    const res = await bosReconnect({
       lnd: (await authenticatedLnd).lnd
     })
-    log && console.log(`${getDate()} bos.runReconnect() complete`, res)
+    log && console.log(`${getDate()} bos.reconnect() complete`, res)
     return res
   } catch (e) {
-    console.error(`\n${getDate()} bos.runReconnect() aborted:`, JSON.stringify(e))
+    console.error(`\n${getDate()} bos.reconnect() aborted:`, JSON.stringify(e))
   }
 }
 
-const runRebalance = async ({
+const rebalance = async ({
   fromChannel,
   toChannel,
   maxSats = 200000,
@@ -38,12 +43,12 @@ const runRebalance = async ({
       out_through: fromChannel,
       in_through: toChannel,
       max_rebalance: String(maxSats),
-      timeout_minutes: maxMinutes,
+      timeout_minutes: Math.trunc(maxMinutes),
       max_fee_rate: maxFeeRate,
-      max_fee: Math.trunc(maxSats * 0.01) // unused, 10k ppm
+      max_fee: Math.trunc(maxSats * 0.010000) // unused, 10k ppm
     }
-    log && console.log(`${getDate()} bos.runRebalance()`, JSON.stringify(options))
-    const res = await rebalance({
+    console.log(`${getDate()} bos.rebalance()`, log ? JSON.stringify(options) : '')
+    const res = await bosRebalance({
       ...options,
       fs: {getFile: readFile}, // required
       lnd: (await authenticatedLnd).lnd,
@@ -51,16 +56,71 @@ const runRebalance = async ({
       avoid: [], // seems necessary
       out_channels: [] // seems necessary
     })
-    log && console.error(`\n${getDate()} bos.runRebalance() success:`, JSON.stringify(res))
+    log && console.log(`\n${getDate()} bos.rebalance() success:`, JSON.stringify(res))
     console.log('')
     return {
       fee_rate: +(res.rebalance[2]?.rebalance_fee_rate.match(/\((.*)\)/)[1]),
       rebalanced: Math.trunc(+(res.rebalance[2]?.rebalanced) * 1e8)
     }
   } catch (e) {
-    console.error(`\n${getDate()} bos.runRebalance() aborted:`, JSON.stringify(e))
+    console.error(`\n${getDate()} bos.rebalance() aborted:`, JSON.stringify(e))
+    // provide suggested ppm if possible
     return {
       failed: true,
+      ppmSuggested: e[1] === 'RebalanceFeeRateTooHigh' ? +e[2].needed_max_fee_rate : null,
+      msg: e
+    }
+  }
+}
+
+const send = async ({
+  destination,
+  fromChannel,
+  toChannel,
+  sats = 1,
+  maxMinutes = 1,
+  maxFeeRate = 100
+}, log = false) => {
+  const options = {
+    amount: String(Math.trunc(sats)),
+    destination: destination,
+    out_through: fromChannel,
+    in_through: toChannel,
+    // uses max fee (sats) only so calculated from max fee rate (ppm)
+    max_fee: Math.trunc(sats * maxFeeRate * 1e-6 + 1),
+    // message: '',
+    timeout_minutes: Math.trunc(maxMinutes)
+  }
+  try {
+    console.log(`${getDate()} bos.send()`, log ? JSON.stringify(options) : '')
+    const res = await bosPushPayment({
+      ...options,
+      fs: {getFile: readFile}, // required
+      lnd: (await authenticatedLnd).lnd,
+      logger: {
+        info: v => log ? console.log(getDate(), v) : process.stdout.write('.')
+      },
+      is_dry_run: false,
+      quiz_answers: [],
+      request
+    })
+    log && console.log(`\n${getDate()} bos.send() success:`, JSON.stringify(res))
+    console.log('')
+    return {
+      fee_rate: Math.trunc(1.0 * +res.fee / +res.paid * 1e6),
+      rebalanced: Math.trunc(+res.paid - +res.fee)
+    }
+  } catch (e) {
+    console.log(`\n${getDate()} bos.send() aborted:`, JSON.stringify(e))
+    // just max fee suggestions so convert to ppm
+    // e.g. [400,"MaxFeeLimitTooLow",{"needed_fee":167}]
+    return {
+      failed: true,
+      ppmSuggested: e[1] === 'MaxFeeLimitTooLow'
+      ? (
+        Math.trunc(1.0 * +e[2].needed_fee / sats * 1e6 + 1.0)
+      )
+      : null,
       msg: e
     }
   }
@@ -70,7 +130,7 @@ const runRebalance = async ({
 const setFees = async (peerPubKey, fee_rate, log = false) => {
   try {
     log && console.log(`${getDate()} bos.setFees()`)
-    const res = await adjustFees({
+    const res = await bosAdjustFees({
       fs: {getFile: readFile}, // required
       lnd: (await authenticatedLnd).lnd,
       logger: {}, // logger not used
@@ -78,6 +138,7 @@ const setFees = async (peerPubKey, fee_rate, log = false) => {
       fee_rate: String(fee_rate) // pm rate to set
     })
     const newFee = res.rows[1][1]?.match(/\((.*)\)/)[1]
+    log && console.log(`${getDate()} bos.setFees()`, JSON.stringify(res), newFee)
     return +newFee
   } catch(e) {
     console.error(`${getDate()} bos.setFees() aborted:`, e)
@@ -107,29 +168,27 @@ const callAPI = async (method, choices = {}) => {
 
 
 // returns {public_key, inbound_liquidity, outbound_liquidity, inbound_fee_rate, alias}
-const getPeers = async (log = false) => {
+const getPeers = async (options = {}, log = false) => {
   try {
-    const res = await bos_network.getPeers({
+    log && console.log(`${getDate()} bos.getPeers()`)
+    const res = await bosGetPeers({
       fs: {getFile: readFile}, // required
       lnd: (await authenticatedLnd).lnd,
       omit: [], // required
       active: true, // only connected peers
-      public: true // only public peers
+      public: true, // only public peers
+      ...options
     })
-    log && console.log(`${getDate()} bos.getPeers()`)
-    // most keys are undefined so only some kept here
-    const keysToShow = (
-      {alias, public_key, inbound_liquidity, outbound_liquidity, inbound_fee_rate}
-    ) => (
-      {alias, public_key, inbound_liquidity, outbound_liquidity, inbound_fee_rate}
-    )
-    return res.peers
-      .map(keysToShow)
-      // convert fee rate to just ppm
-      .map(peer => ({
-        ...peer,
-        inbound_fee_rate: peer.inbound_fee_rate?.match(/\((.*)\)/)[1]
-      }))
+    const peers = res.peers
+
+    // convert fee rate to just ppm
+    .map(peer => ({
+      ...peer,
+      inbound_fee_rate: peer.inbound_fee_rate?.match(/\((.*)\)/)[1]
+    }))
+
+    log && console.log(`${getDate()} bos.getPeers()`, JSON.stringify(peers, (k, v) => v === undefined ? 'undefined' : v, 2))
+    return peers
   } catch (e) {
     console.error(`${getDate()} bos.getPeers() aborted:`, e)
     return []
@@ -140,7 +199,7 @@ const getPeers = async (log = false) => {
 const getFees = async (log = false) => {
   try {
     log && console.log(`${getDate()} bos.getFees()`)
-    const res = await adjustFees({
+    const res = await bosAdjustFees({
       fs: {getFile: readFile}, // required
       lnd: (await authenticatedLnd).lnd,
       logger: {}, // logger not used
@@ -162,12 +221,15 @@ const getFees = async (log = false) => {
 
 const getDate = timestamp => (timestamp ? new Date(timestamp) : new Date()).toISOString()
 
+const request = ({}, cbk) => cbk(null, {}, {})
+
 const bos = {
   getPeers,
   callAPI,
   getFees,
   setFees,
-  runRebalance,
-  runReconnect
+  rebalance,
+  reconnect,
+  send
 }
 export default bos
