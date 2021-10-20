a140b4a15318785fdbe14199bcea44754916c9d3
legalizemath <89911237+legalizemath@users.noreply.github.com>
2021-10-20 03:50:37 -0400

    xcv

--- backupPayments.js
+++ backupPayments.js
@@ -2,21 +2,18 @@ import fs from 'fs'
 import bos from './bos.js'
 
 const LOG_FILES = './logs'
-const DAYS_FOR_STATS = 999 // enough to capture all payments needed before deletion
+const DAYS_FOR_STATS = 999 // lose any days not backed up
 
 const initialize = async () => {
-  // make folder if necessary
-  if (!fs.existsSync(LOG_FILES)) {
-    fs.mkdirSync(LOG_FILES, { recursive: true })
-  }
-  // get payments
+  if (!fs.existsSync(LOG_FILES)) fs.mkdirSync(LOG_FILES, { recursive: true })
+
   const payments = await bos.customGetPaymentEvents({ days: DAYS_FOR_STATS })
-  // put payments into a file with timestamp in name
   fs.writeFileSync(`${LOG_FILES}/${Date.now()}_paymentHistory.json`, JSON.stringify(payments, fixJSON, 2))
   console.log(`${payments.length} payments backed up`)
-  // clear payments from database
+
   const res = await bos.callAPI('deletePayments')
-  console.log('all payments deleted from database', res)
+  console.log('all payments deleted from database', res || '')
 }
+
 const fixJSON = (k, v) => (v === undefined ? null : v)
 initialize()
