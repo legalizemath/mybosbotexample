8bf3c2f33919900aaf33a770bef4dcfd0c34468d
legalizemath <89911237+legalizemath@users.noreply.github.com>
2021-11-13 21:27:09 -0500

    can feed it non-peer public keys

--- isitdown.js
+++ isitdown.js
@@ -1,6 +1,9 @@
 // need to run npm link balanceofsatoshis in folder & bos.js & package.json with {"type": "module"}
 // run for peers like this: `node isitdown silk node` or `npm link balanceofsatoshis && node isitdown acinq`
-// returns something like `2.0% of 49 channels disabled towards Silk Node 🐪`
+// returns something like 
+// `2.0% of 49 channels disabled towards Silk Node 🐪`
+// node that's down for many might look like this
+// `~64.7% of 17 channels disabled towards 0279c1c9df1a7289c14e`
 
 import bos from './bos.js'
 const matchString = process.argv.slice(2).join(' ')
@@ -8,13 +11,15 @@ const matchString = process.argv.slice(2).join(' ')
 const run = async () => {
   const peers = await bos.peers({ is_active: undefined, is_public: undefined })
 
-  const peer = peers.find(
+  let peer = peers.find(
     p =>
       p.alias.toLowerCase() === matchString.toLowerCase() ||
       p.public_key === matchString ||
       p.alias.toLowerCase().includes(matchString.toLowerCase()) ||
       p.public_key.includes(matchString)
   )
+  // maybe just public key
+  if (!peer && matchString.length === 66) peer = { alias: 'unknown alias', public_key: matchString }
 
   if (!peer) return console.log('peer not found')
 
