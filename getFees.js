9c2174bd34a36fa2cf7ac1e22b54d2789ba5753a
legalizemath <89911237+legalizemath@users.noreply.github.com>
2022-01-08 19:42:00 -0500

    example to get fees to/from every channel/peer

---
+++ getFees.js
@@ -0,0 +1,21 @@
+// needs bos.js and package.json with {"type": "module"} in same folder
+// run with "npm link balanceofsatoshis && node getRemoteFees
+
+import bos from './bos.js'
+
+const run = async () => {
+  const peers = await bos.peers() // active peers
+  const getFeeRates = await bos.getNodeChannels({ byPublicKey: true })
+  for (const peer of peers) {
+    getFeeRates[peer.public_key].forEach(channel => {
+      const outgoingFeeRate = channel.local.fee_rate + '  ppm'
+      const outgoingBaseFee = channel.local.base_fee_mtokens + ' msats'
+      const incFeeRate = channel.remote.fee_rate + '  ppm'
+      const incBaseFee = channel.remote.base_fee_mtokens + ' msats'
+      console.log(peer.alias, peer.public_key, channel.id)
+      console.log(`local:  ${outgoingFeeRate.padStart(10)}  ${outgoingBaseFee.padStart(10)}`)
+      console.log(`remote: ${incFeeRate.padStart(10)}  ${incBaseFee.padStart(10)}`)
+    })
+  }
+}
+run()
