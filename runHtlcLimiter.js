a360dc5c8eb4d7cb8c35a900173789081b69f959
legalizemath <89911237+legalizemath@users.noreply.github.com>
2022-02-13 14:51:53 -0500

    note

--- runHtlcLimiter.js
+++ runHtlcLimiter.js
@@ -1,4 +1,5 @@
 // just uses htlcLimiter.js so I don't have to keep 2 versions of same code around
+// runs it via `npm link balanceofsatoshis && node runHtlcLimiter`
 
 import htlcLimiter from './htlcLimiter.js'
 
